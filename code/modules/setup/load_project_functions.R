67651e3e192d10dbedafed9f17792849a03f57f0
harel <harel@harmonic.ac.nz>
2018-08-14 15:26:08 +1200

    created the basic folder structure

---
+++ code/modules/setup/load_project_functions.R
@@ -0,0 +1,47 @@
+################################################################################
+#                           Load Project's Functions                           #
+################################################################################
+#'
+load_project_functions <- function() {
+  ####################
+  # Input validation #
+  ####################
+  #' Check that the required parameters exist
+  stopifnot(
+    exists("k_path_functions"),
+    exists("k_path_modules")
+  )
+  #'
+  ############################
+  # Load project's functions #
+  ############################
+  invisible(
+    sapply(
+      list.files(k_path_functions,
+        pattern = "*.R$", full.names = TRUE, recursive = TRUE,
+        ignore.case = TRUE
+      ),
+      source,
+      local = globalenv()
+    )
+  )
+  #'
+  ##########################
+  # Load project's modules #
+  ##########################
+  invisible(
+    sapply(
+      list.files(k_path_modules,
+        pattern = "*.R$", full.names = TRUE, recursive = TRUE,
+        ignore.case = TRUE
+      ),
+      source, 
+      local = globalenv()
+    )
+  )
+  #'
+  ##########
+  # Return #
+  ##########
+  return(invisible())
+}
---
+++ code/modules/setup/load_project_parameters.R
@@ -0,0 +1,16 @@
+################################################################################
+#                             Project's Parameters                             #
+################################################################################
+#'
+#' The project's parameters are assigned as global variables and carry the
+#' prefix k_ to help the user to identify them as such.
+#'
+load_project_parameters <- function() {
+  #' Number of cores to use in this project
+  k_ncores <<- 1
+  #'
+  ##########
+  # Return #
+  ##########
+  return(invisible())
+}
