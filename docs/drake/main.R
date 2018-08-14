33448acd051a9ebfa66924683d254fdbc3321e2e
harel <harel@harmonic.ac.nz>
2018-08-14 15:57:54 +1200

    added main skeleton for drake

---
+++ docs/drake/main.R
@@ -0,0 +1,18 @@
+################################################################################
+#                                     Main                                     #
+################################################################################
+#' GitHub webpage
+#' <https://ropensci.github.io/drake/>
+#' 
+#' The drake R Package User Manual
+#' <https://ropenscilabs.github.io/drake-manual/index.html#the-drake-r-package>
+#' 
+#####################
+# Inputs validation #
+#####################
+k_path_project <<- rprojroot::find_rstudio_root_file()
+source(file.path(k_path_project, "code", "scripts", "setup.R"))
+#'
+#######
+# ??? #
+#######
