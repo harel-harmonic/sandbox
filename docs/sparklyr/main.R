b675921bcf5c4caa1be545d5d65e4825f3163d9f
harel <harel@harmonic.ac.nz>
2018-08-16 13:48:38 +1200

    install spark for development purposes

--- docs/sparklyr/main.R
+++ docs/sparklyr/main.R
@@ -2,8 +2,12 @@
 #                                    Main                                      #
 ################################################################################
 #'
-#####################
-# Inputs validation #
-#####################
+#########
+# Setup #
+#########
+# Run generic setup
 k_path_project <<- rprojroot::find_rstudio_root_file()
 source(file.path(k_path_project, "code", "scripts", "setup.R"))
+# Install a local version of Spark for development purposes
+if(nrow(spark_installed_versions()) == 0)
+    spark_install(version = "2.3.0")
