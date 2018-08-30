9343d357a3f8679338215df3b0d32085cdcf3e1d
harel <harel@harmonic.ac.nz>
2018-08-31 11:18:44 +1200

    second version of moving block bootstrap sample

--- code/R/bootstraps_moving_block.R
+++ code/R/bootstraps_moving_block.R
@@ -1,39 +1,37 @@
 #' bootstraps_moving_block
 #'
-#' @param block_complete "both", "left" or "right"; 
-#' the blocks in the begining and end are problemtic, as they contain less 
-#' than \code{block_size} observations. Specifying this parameter ensures the desired ends
-#' is sameple among \code{block_size} observations. 
-#' 
-#' @note \code{block_complete}="rigth" might be desirable in some cases as it 
-#' ensures the closer the sample gets to the end of the series, it is more  
-#' likely to get the real observed value
-#' 
-bootstraps_moving_block <- function(data, 
-                                    times = 25, 
-                                    block_size){
+#' @param first_split_real_data a logical. If TRUE (default), the first split is
+#' the original (unsampled) data.
+#'
+bootstraps_moving_block <- function(data,
+                                    times = 25,
+                                    block_size,
+                                    first_split_real_data = TRUE) {
     ####################
     # Input validation #
     ####################
     stopifnot(!missing(data), !missing(block_size))
     assert_is_data.frame(data)
     assert_all_are_positive(c(times, block_size))
+    assert_is_logical(first_split_real_data)
     #'
     #########
     # Setup #
     #########
     n <- nrow(data)
-    block_quantity <- n / block_size
-    # TODO handle sequence where the number of instances is not a multiplication of the block size
-    stopifnot(block_quantity == round(block_quantity))
+    block_quantity <- ceiling(n / block_size)
     #'
     #############################
     # Create bootstrap skeleton #
     #############################
+    # Create a strata variable which associates instances and blocks
+    strata <- rep(1:block_quantity, each = block_size)
+    # In case one of the blocks isn't fully populated, make sure it's the
+    # first-block (rather than the last block)
+    m <- block_quantity * block_size
+    strata <- strata[(m - n + 1):m]
     # Add strata column to the data
-    data <- 
-        data %>%
-        add_column(strata = rep(1:block_quantity, each = block_size), .before = 1)
+    data <- data %>% add_column(strata = strata, .before = 1)
     # Create the skeleton for the time series bootstrap
     rset <- rsample::bootstraps(data, times, "strata")
     #'
@@ -41,37 +39,50 @@ bootstraps_moving_block <- function(data,
     # Moving block bootstrap #
     ##########################
     # Find the indices of each block
-    block_indices <- sapply(1:(n-block_size+1),
-                            function(x) seq.int(from = x, to = x + (block_size-1)))
+    block_indices <- sapply(
+        1:(n - block_size + 1),
+        function(x) seq.int(from = x, to = x + (block_size - 1))
+    )
     block_indices <- t(block_indices)
     # Complement the last incomplete blocks
-    block_indices <- rbind(block_indices,
-                           block_indices[rep(nrow(block_indices), block_size-1),])
+    block_indices <- rbind(
+        block_indices,
+        block_indices[rep(nrow(block_indices), block_size - 1), ]
+    )
+    # Revert the prevalence of the observation. This makes sure the freshest
+    # observations are not available for bootstrap sampling before they have
+    # appeared
+    block_indices <- max(block_indices) + 1 - block_indices
+    block_indices <- block_indices[nrow(block_indices):1, ncol(block_indices):1]
     # Convert data frame to list
     block_indices <- split(block_indices, block_indices %>% nrow() %>% seq())
     #'
     ########################
     # Bootstrap the sample #
     ########################
-    bootstrap_sampling <- function(x,...){
-        if(length(x)==1){ # if the sample size is one, then return the sample value
+    bootstrap_sampling <- function(x, ...) {
+        if (length(x) == 1) { # if the sample size is one, then return the sample value
             x
         } else { # else sample with replacement
             base::sample(x, 1, replace = TRUE)
         }
-    }# bootstrap_sampling
+    } # bootstrap_sampling
     #'
     #################################################
     # Set the indices inside the bootstrap skeleton #
     #################################################
-    for(i in 1:times){
+    for (i in 1:times) {
         # Bootstrap the sample
-        in_id <- map_int(block_indices, bootstrap_sampling) %>% unname() 
+        in_id <- map_dbl(block_indices, bootstrap_sampling) %>% unname()
         # Store the bootstrap sample in rset object
-        rset$splits[[i]]$in_id <- in_id
+        rset$splits[[i]]$in_id <- in_id %>% as.integer()
         # Remove strata column form data
         rset$splits[[i]]$data <- rset$splits[[i]]$data %>% select(-strata)
     }
+    # Should the unsampled data be retained in the first split?
+    if (first_split_real_data) {
+          rset$splits[[1]]$in_id <- 1:n
+      }
     #'
     ##########
     # Return #
--- docs/rsample/bootstrap-methods-for-time-series.R
+++ docs/rsample/bootstrap-methods-for-time-series.R
@@ -3,22 +3,22 @@
 ################################################################################
 #'
 #' # Introduction
-#' 
+#'
 #' There are (at least) four methods:
-#' 
+#'
 #' (1) Non-overlapping block bootstrap
 #' (2) Overlapping block bootstrap (moving block bootstrap)
-#' (3) Stationary block bootstrap 
+#' (3) Stationary block bootstrap
 #' (4) Subsampling
-#' 
+#'
 #' This scripts contain code for the first and second methods.
 #'
 #' ---
-#' 
+#'
 #' # Bootstrapping Time Series Data: Some Limitations
-#' 
+#'
 #' * Problems with sample: non-representative, too small
-#' * Problems from dependency structure: 
+#' * Problems from dependency structure:
 #'     * wrong dependency assumption
 #'     * regime changes
 #'     * long-term dependency
@@ -26,13 +26,13 @@
 #' * Problems with certain statistics: “Edge” statistics may require many, many
 #'   replicates
 #' * Finally, Monte Carlo may be better alternative.
-#' 
+#'
 #' ---
-#' 
+#'
 #' # (2) Moving block bootstrap
-#' 
+#'
 #' Moving block Bootstrap preserves the local dependency structure.
-#' 
+#'
 #' * Break time series into little blocks.
 #' * Resample the blocks, not individual points – kind of "random shuffling,
 #'   with replacement.
@@ -45,7 +45,7 @@
 # Load Project Libraries and Functions
 k_path_project <<- rprojroot::find_rstudio_root_file()
 source(file.path(k_path_project, "code", "scripts", "setup.R"))
-# What is the structure of the time series? 
+# What is the structure of the time series?
 block_quantity <- 24 # There are 24 hours in the time series
 block_size <- 6 # There are 6 evenly spread samples within an hour = 10 minutes
 n <- block_quantity * block_size # Total number of observations in the series
@@ -55,16 +55,24 @@ times <- 10
 #################
 # Generate data #
 #################
-dummy_data <- data.frame(sample_id = 1:n,
-                         hour_id = rep(1:block_quantity, each = block_size),
-                         value = -n:-1)
+dummy_data <- data.frame(
+    sample_id = 1:n,
+    hour_id = rep(1:block_quantity, each = block_size),
+    value = -n:-1
+)
+dummy_data <- dummy_data[1:(n - 3), ]
 head(dummy_data)
 #'
 ###################
 # Sample the data #
 ###################
 set.seed(1135)
-rset <- bootstraps_moving_block(data = dummy_data, times, block_size)
+rset <- bootstraps_moving_block(
+    data = dummy_data,
+    times = times,
+    block_size = block_size,
+    first_split_real_data = TRUE
+)
 #'
 ###########################
 # Explore the rset object #
@@ -84,4 +92,4 @@ rset$splits[[times]]$in_id
 rset$splits[[1]]$out_id
 rset$splits[[2]]$out_id
 # ...
-rset$splits[[times]]$in_id
+rset$splits[[times]]$out_id
