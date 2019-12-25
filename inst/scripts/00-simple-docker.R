4df1df790bbe817a0d8053936fabcac5706523d6
harell <harel@loostiger.com>
2019-12-26 12:32:15 +1300

    run rstudio-server

--- inst/scripts/00-simple-docker.R
+++ inst/scripts/00-simple-docker.R
@@ -1,5 +1,6 @@
 # Setup -------------------------------------------------------------------
 # remotes::install_github("colinfay/dockerfiler")
+# remotes::install_github("wch/harbor")
 get_docker <- function(Dockerfile){
     file_path <- tempfile("docker")
     docker01$write(file_path)
@@ -7,8 +8,32 @@ get_docker <- function(Dockerfile){
     message("Saved commends to ", basename(file_path))
 }
 
+# Docker Commands ---------------------------------------------------------
+# 1. See available docker commands
+system("docker")
+# 2. List available docker images on the machine
+system("docker images")
+# 3. Get R image from Docker Hub
+system("docker pull rocker/verse")
+# 4. Run RStudio Server
+system("docker run -e PASSWORD=1234 --rm -p 8787:8787 rocker/verse")
+browseURL("http://127.0.0.1:8787")
+message("| Username: rstudio | Password: 1234 |")
+
+
+
+
+
 # 01 Simple Docker File ---------------------------------------------------
+file_path <- tempfile(fileext = ".docker")
 docker01 <- dockerfiler::Dockerfile$new()
-get_docker(docker01)
+docker01$write(file_path)
+paste("docker build", fs::path_tidy(dirname(file_path)), "-f", basename(file_path))  
+shell.exec(dirname(file_path))
 
 
+# harbor::docker_run(image = "docker01", cmd = docker01$print(), capture_text = TRUE)
+# 
+command <- "docker run -it -d"
+
+paste(command, fs::path_tidy(dirname(file_path)), "-f", basename(file_path))  
